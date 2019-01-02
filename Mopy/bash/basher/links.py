d69f24e797c1c453b3f154730363d9e95241aa75
MrD <the.ubik@gmail.com>
2019-01-02 18:40:01 -0500

    Enabling mod export for SSE - XXXXX
    leftover from previous probably depends on more changes in the
    "Frankenstein build" a0e7a6d715b136af6d8d180e4133a98839faaffa

--- Mopy/bash/basher/links.py
+++ Mopy/bash/basher/links.py
@@ -491,7 +491,7 @@ def InitModLinks():
             exportMenu.links.append(CBash_Mod_CellBlockInfo_Export())
             exportMenu.links.append(Mod_EditorIds_Export())
             ## exportMenu.links.append(Mod_ItemData_Export())
-            if bush.game.fsName == u'Skyrim':
+            if bush.game.fsName[:6] == u'Skyrim':
                 exportMenu.links.append(Mod_FullNames_Export())
                 exportMenu.links.append(Mod_Prices_Export())
                 exportMenu.links.append(Mod_Stats_Export())
@@ -526,7 +526,7 @@ def InitModLinks():
             importMenu = MenuLink(_(u"Import"))
             importMenu.links.append(Mod_EditorIds_Import())
             ## importMenu.links.append(Mod_ItemData_Import())
-            if bush.game.fsName == u'Skyrim':
+            if bush.game.fsName[:6] == u'Skyrim':
                 importMenu.links.append(Mod_FullNames_Import())
                 importMenu.links.append(Mod_Prices_Import())
                 importMenu.links.append(Mod_Stats_Import())
