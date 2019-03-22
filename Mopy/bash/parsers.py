458d307a06c3e869afe3c73ebb996e5195a97697
Infernio <infernio@icloud.com>
2019-03-22 17:38:27 +0100

    WIP: Use FileInfo.get_masters() where possible
    Note: This relies on the cosaves rework, specifically the 'cosaves.py -
    beta' commit and the 'correct save masters' commit.
    
    Much easier to find the definition and search usages for than the
    previous '.header.masters' code, which is now only used where necessary.
    Will also allow us to be much more flexible in the future if masters
    become more complex than that.
    
    Obviously touches a ton of places, so WIP, but it shouldn't have any
    functional impacts.

--- Mopy/bash/parsers.py
+++ Mopy/bash/parsers.py
@@ -64,7 +64,7 @@ class ActorFactions:
     def readFactionEids(self,modInfo):
         """Extracts faction editor ids from modInfo and its masters."""
         loadFactory = LoadFactory(False,MreRecord.type_class['FACT'])
-        for modName in (modInfo.header.masters + [modInfo.name]):
+        for modName in (modInfo.get_masters() + [modInfo.name]):
             if modName in self.gotFactions: continue
             modFile = ModFile(bosh.modInfos[modName],loadFactory)
             modFile.load(True)
@@ -304,7 +304,7 @@ class ActorLevels:
         """Imports actor level data from the specified mod and its masters."""
         mod_id_levels, gotLevels = self.mod_id_levels, self.gotLevels
         loadFactory = LoadFactory(False,MreRecord.type_class['NPC_'])
-        for modName in (modInfo.header.masters + [modInfo.name]):
+        for modName in (modInfo.get_masters() + [modInfo.name]):
             if modName in gotLevels: continue
             modFile = ModFile(bosh.modInfos[modName],loadFactory)
             modFile.load(True)
@@ -823,7 +823,7 @@ class FactionRelations:
     def readFactionEids(self,modInfo):
         """Extracts faction editor ids from modInfo and its masters."""
         loadFactory = LoadFactory(False,MreRecord.type_class['FACT'])
-        for modName in (modInfo.header.masters + [modInfo.name]):
+        for modName in (modInfo.get_masters() + [modInfo.name]):
             if modName in self.gotFactions: continue
             modFile = ModFile(bosh.modInfos[modName],loadFactory)
             modFile.load(True)
